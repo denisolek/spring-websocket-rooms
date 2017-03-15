0cb23417978f090c86652d7385e20515edd35c94
deniso95 <denis.olek@gmail.com>
2017-03-15 20:17:17 +0100

    init

---
+++ src/main/java/pl/denisolek/ChatRoomsApplication.java
@@ -0,0 +1,12 @@
+package pl.denisolek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRoomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatRoomsApplication.class, args);
+	}
+}
