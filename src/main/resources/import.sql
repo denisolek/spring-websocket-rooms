7f799a128c9009d122fbb50d6c979d196a28d41b
Denis Olek <denis.olek@gmail.com>
2017-03-16 17:09:12 +0100

    Added import sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,15 @@
+insert into projects(id, name, client_company, executive_company) values (1, 'Name 1', 'Client Company 1', 'Executive Company 1');
+insert into projects(id, name, client_company, executive_company) values (2, 'Name 2', 'Client Company 2', 'Executive Company 2');
+insert into projects(id, name, client_company, executive_company) values (3, 'Name 3', 'Client Company 3', 'Executive Company 3');
+insert into projects(id, name, client_company, executive_company) values (4, 'Name 4', 'Client Company 4', 'Executive Company 4');
+insert into projects(id, name, client_company, executive_company) values (5, 'Name 5', 'Client Company 5', 'Executive Company 5');
+insert into projects(id, name, client_company, executive_company) values (6, 'Name 6', 'Client Company 6', 'Executive Company 6');
+insert into projects(id, name, client_company, executive_company) values (7, 'Name 7', 'Client Company 7', 'Executive Company 7');
+insert into projects(id, name, client_company, executive_company) values (8, 'Name 8', 'Client Company 8', 'Executive Company 8');
+insert into projects(id, name, client_company, executive_company) values (9, 'Name 9', 'Client Company 9', 'Executive Company 9');
+insert into projects(id, name, client_company, executive_company) values (10, 'Name 10', 'Client Company 10', 'Executive Company 10');
+insert into projects(id, name, client_company, executive_company) values (11, 'Name 11', 'Client Company 11', 'Executive Company 11');
+insert into projects(id, name, client_company, executive_company) values (12, 'Name 12', 'Client Company 12', 'Executive Company 12');
+insert into projects(id, name, client_company, executive_company) values (13, 'Name 13', 'Client Company 13', 'Executive Company 13');
+insert into projects(id, name, client_company, executive_company) values (14, 'Name 14', 'Client Company 14', 'Executive Company 14');
+insert into projects(id, name, client_company, executive_company) values (15, 'Name 15', 'Client Company 15', 'Executive Company 15');
