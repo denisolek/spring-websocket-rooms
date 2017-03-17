fc6897ea3deb51f0435b63b5fe2405f7987ed22c
Denis Olek <denis.olek@gmail.com>
2017-03-17 11:43:30 +0100

    Changed ChatMessage class to entity

--- src/main/java/pl/denisolek/model/ChatMessage.java
+++ src/main/java/pl/denisolek/model/ChatMessage.java
@@ -1,7 +1,27 @@
 package pl.denisolek.model;
 
+import pl.denisolek.entities.Project;
+import pl.denisolek.entities.User;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "chat_messages")
 public class ChatMessage {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
     private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
     public ChatMessage() {
     }
